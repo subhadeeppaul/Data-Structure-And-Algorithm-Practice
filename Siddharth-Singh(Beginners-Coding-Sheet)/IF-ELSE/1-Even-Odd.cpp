d0bbef97f5dfc63cfaf16db12ce556bf5f0f7b95
Subhadeep Paul <subhadeeppaul35@gmail.com>
2023-10-01 14:37:15 +0530

    Odd even program added

---
+++ Siddharth-Singh(Beginners-Coding-Sheet)/IF-ELSE/1-Even-Odd.cpp
@@ -0,0 +1,18 @@
+// Write a Program to Check Whether Number is Even or odd
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<n<<" is even";
+    }
+    else{
+        cout<<n<<" is odd";
+    }
+    
+}
