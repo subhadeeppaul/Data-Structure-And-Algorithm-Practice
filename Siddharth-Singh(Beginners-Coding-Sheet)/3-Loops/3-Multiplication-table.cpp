be947c869679ffc8075c339f1895580ac3c995da
Subhadeep Paul <subhadeeppaul35@gmail.com>
2023-10-01 23:23:11 +0530

    Multiplication table added

---
+++ Siddharth-Singh(Beginners-Coding-Sheet)/3-Loops/3-Multiplication-table.cpp
@@ -0,0 +1,15 @@
+// Write a Program to Generate Multiplication Table of anumber (entered by the user) using a for loop.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+   int n;
+   cin>>n;
+
+   for(int i=1;i<=10;i++){
+   cout<<n*i<<endl;
+   }
+
+   
+}
