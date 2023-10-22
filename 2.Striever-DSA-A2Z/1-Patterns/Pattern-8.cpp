2be2b581a234eddfa2cf90182d4147ae2bcfb8b8
Subhadeep Paul <subhadeeppaul35@gmail.com>
2023-10-22 21:35:15 +0530

    Pattern-8 Added

---
+++ 2.Striever-DSA-A2Z/1-Patterns/Pattern-8.cpp
@@ -0,0 +1,40 @@
+/* n=5
+ * * * * * * * * *
+   * * * * * * * 
+   * * * * * 
+     * * * 
+       *   
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+void pat8(int n){
+    
+    for(int i=0;i<n;i++){
+
+        //space
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+
+        //stars
+        for(int j=0;j<2*n-2*i-1;j++){
+            cout<<"* ";
+        }
+
+        //space
+         for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+
+        cout<<endl;
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    pat8(n);
+
+}
